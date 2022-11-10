d589e30a76f634f56b09fccf95b719c2f160f8de
Dario Turin <dturin1997@gmail.com>
2022-11-10 10:48:01 -0500

    Se agregó atlas conección

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGO_USERNAME = 'mongo';
-const MONGO_PASSWORD = 'mongo';
+const MONGO_USERNAME = 'root';
+const MONGO_PASSWORD = 'root';
 const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'ddseln_lab_s9_crud';
+//const MONGO_PORT = '27017';
+//const MONGO_DB = 'ddseln_lab_s9_crud';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
+//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url =`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.mjsqwcg.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
 
 const db = mongoose.connection
